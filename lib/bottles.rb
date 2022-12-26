4cb15d1ea8354e2e83cf39f67bbac7c2ac5e9862
Riley Hoock <laurenh8916@protonmail.com>
2022-12-25 17:00:20 -0700

    verse implemented

---
+++ lib/bottles.rb
@@ -0,0 +1,11 @@
+class Bottles 
+  # def initialize(params)  
+
+  # end
+
+  def verse(count)
+
+     "#{count} bottles of beer on the wall, #{count} bottles of beer.\nTake one down and pass it around, #{count-1} bottles of beer on the wall.\n"
+
+  end
+end
